22ecd79d4d166757dfdcee1a77d5d317a718b154
Marian Korosec <kormam13@htl-kaindorf.ac.at>
2017-09-29 15:14:29 +0200

    Fixed Case Sensitivity Bug

--- quickstart_fundamentals/src/app/FormsFundamental/hero.service.ts
+++ quickstart_fundamentals/src/app/FormsFundamental/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/Observable/of';
+import { Observable } from 'rxjs/observable';
+import { of } from 'rxjs/observable/of';
 import 'rxjs/add/operator/delay';
 
 import { ReactiveHero, reactiveHeroes } from '../core/hero';
